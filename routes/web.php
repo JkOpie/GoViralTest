01c87b1b237edd0b6592eca227eafd43d04395cd
JkOpie <assyaafi96@gmail>
2022-05-30 10:49:04 +0800

    add necessary route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderItemController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [OrderController::class, 'index']);
+
+Route::resources([
+    'order' => OrderController::class,
+    'orderitems'=> OrderItemController::class,
+    'transaction'=> TransactionController::class
+]);
+
+Route::get('/calc-total/{id}', [OrderController::class, 'calc_total']);
